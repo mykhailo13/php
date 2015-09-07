c54f69c1c268f64ff36f246d46b24bbf089c0dec
mykhailo13 <mikevkman@gmail.com>
2015-09-07 12:38:30 +0300

    Init project

---
+++ test.php
@@ -0,0 +1,44 @@
+
+
+
+<?php
+//echo 'hello world<br>';
+$count = 5;
+
+$price = 123;
+
+$totalPrice = $count * $price;
+
+//echo 'Total price' . $totalPrice . '$';
+
+
+
+
+// boolean: true false 
+$name = 'Mykhailo';
+
+$isBlocked = false;
+
+if ($isBlocked) {
+	echo "$name You are blocked<br>";
+} else {
+	echo "$name welcome<br>";
+}
+
+echo "Total price: $totalPrice" . '$<br>';
+
+if ($totalPrice >= 1000 or $totalPrice == 500) {
+	echo 'You have 25% discount<br>';
+} elseif ($totalPrice >= 600) {
+	echo 'You have 15% discount<br>';
+} elseif ($totalPrice >= 400) {
+	echo 'You have 10% discount<br>';
+} else { 
+	echo 'You have 5% discount<br>';
+}
+
+if ($count === '5') {
+	echo "Count: $count<br>";
+}
+
+?>
