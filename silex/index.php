fab6f87d60f7a94cfe24a819b5c7e6449234880b
mykhailo13 <mikevkman@gmail.com>
2015-09-21 17:50:15 +0300

    Last files

--- silex/index.php
+++ silex/index.php
@@ -35,13 +35,13 @@ $app->get('/', function() use ($app) {
         'name' => $name,
         'age' => 19,
     ));
-});
+})->bind('homepage');
 
 
 //contact page 
 $app->get('/contact', function() use ($app) {
      return $app['twig']->render('concatc.twig');
-});
+})->bind('contact');
 
 
 
@@ -50,6 +50,38 @@ $app->get('/hello/{name}', function ($name) use ($app) {
         'name' => $name,
     ));
 });
+//Blog page
+$app->get('/blog', function () use ($app) {
+    $conn = $app['db'];
+    $posts = $conn->fetchAll("SELECT * FROM posts");
+    $twig = $app['twig'];
+
+         return $app['twig']->render('blog/index.twig', array('posts'=>$posts));
+})->bind('blog');
+
+//Add post
+$app->get('/blog/add_post', function () use ($app) {
+    
+    $twig = $app['twig'];
+
+         return $app['twig']->render('blog/add_post.twig');
+})->bind('add_post');
+
+$app->post('/blog/add_post', function (Symfony\Component\HttpFoundation\Request $request) use ($app) {
+    $twig = $app['twig'];
+    $conn = $app['db'];
+    $post = $conn->insert("posts", array("heading"=>$request->get('heading'),
+     "content"=>$request->get('content')));
+    $error = 0;
+    if ($error) {
+        return $twig->render('blog/add_post.twig', array('error'=>$error));
+    }
+
+
+         return $app->redirect($app['url_generator']->generate('add_post'));
+});
+
+
 
 // users 
 $app->get('/users', function () use ($app) {
@@ -63,6 +95,11 @@ $app->get('/users', function () use ($app) {
     		'users' => $users
     	));
 });
+
+
+//URL Generator
+$app->register(new Silex\Provider\UrlGeneratorServiceProvider());
+
 
 
 
